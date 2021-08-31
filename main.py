5e724e85a6568ea4e7921b0fd7b071c9d2a1d193
CaineCorso <danielz.boros@gmail.com>
2021-08-31 13:43:04 +0200

    Create main.py
    an attempt for separating functions.

---
+++ main.py
@@ -0,0 +1,5 @@
+def main():
+    # The main function of the game, within it, is the while function (play) that runs the game itself, if user types
+    # 'quit' the exit function is called.
+    pass
+    # while
